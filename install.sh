5cc4c954174801434084d277c6070b88862e3c79
Zuzanna Hartleb <nemusa@gmail.com>
2016-08-21 14:56:25 +0200

    Added global gitignore

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 git pull
-rsync --exclude ".git/" --exclude "install.sh" --exclude "README.md" -av . ~
+rsync --exclude ".gitignore_global" --exclude ".git/" --exclude "install.sh" --exclude "README.md" -av . ~
+
+# Git setup
+rsync -v .gitignore_global ~/.gitignore
+git config --global core.excludesfile ~/.gitignore
 
 # Set up some directory shortcuts
 PROJECT_HOME=$HOME/Projects
@@ -9,4 +13,4 @@ ln -sfv $HOME/.ssh/config $PROJECT_HOME/ssh_config
 ln -sfv $HOME/.ssh/known_hosts $PROJECT_HOME/known_hosts
 
 # Sublime Text setup
-cp Preferences.sublime-settings $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
+rsync -v Preferences.sublime-settings $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
