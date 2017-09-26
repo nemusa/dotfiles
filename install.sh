5b834ce6c8b28d6d5f0a0220303e50334dbaa415
Zuzanna Hartleb <z.hartleb@clearcode.cc>
2017-09-26 16:46:58 +0200

    Added developer tools installation

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@
 sudo easy_install pip
 sudo pip install virtualenv
 
-git clone https://github.com/pyenv/pyenv.git
-git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+xcode-select --install
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# git clone https://github.com/pyenv/pyenv.git
+# git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+
+# /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
