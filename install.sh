63621363471a223544a23ed4de3f58057c5bf064
Zuzanna Hartleb <z.hartleb@clearcode.cc>
2017-10-11 11:57:49 +0200

    Pyenv Python installation as framework

--- install.sh
+++ install.sh
@@ -7,4 +7,6 @@ xcode-select --install
 git clone https://github.com/pyenv/pyenv.git
 git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
 
+PYTHON_CONFIGURE_OPTS="--enable-framework" pyenv install 3.5.0
+
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
