68f2d18f6ddf886c2db3e0f77aaf5d7ec570e48a
Zuzanna Hartleb <z.hartleb@clearcode.cc>
2017-11-20 11:37:05 +0100

    OpenSSL + global .gitignore

--- install.sh
+++ install.sh
@@ -7,6 +7,8 @@ xcode-select --install
 git clone https://github.com/pyenv/pyenv.git
 git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
 
+CFLAGS="-I$(brew --prefix openssl)/include" \
+LDFLAGS="-L$(brew --prefix openssl)/lib" \
 PYTHON_CONFIGURE_OPTS="--enable-framework" pyenv install 3.5.0
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
